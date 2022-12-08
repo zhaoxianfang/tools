107fcfc054737a7ff0061bbca2c0e4b62341d2e9
zhaoxianfang <1748331509@qq.com>
2022-12-08 18:50:17 +0800

    新增Mysql 操作类

---
+++ config/mysql.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * mysqli配置参数
+ */
+return [
+    //  默认连接
+    'default' => [
+        'host'     => '127.0.0.1',
+        'username' => '',
+        'password' => '',
+        'db'       => 'test',
+        'port'     => 3306,
+        'prefix'   => '',
+        'charset'  => 'utf8'
+    ]
+];
---
+++ src/Mysqli/README.md
@@ -0,0 +1,4 @@
+# 手册
+ 
+> 该类由 http://github.com/joshcam/PHP-MySQLi-Database-Class 仓库 改造而来
+

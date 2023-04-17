9533dae1bbb04d5266e888c0467ce1819f2c2d45
zhaoxianfang <1748331509@qq.com>
2023-04-17 18:02:30 +0800

    fix

---
+++ src/tools/TextToImg.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace zxf\tools;
+
+// 使用php8实现的文字生成图片的高级封装类，可以动态设置文字颜色、图片背景色、可以根据图片大小自动计算文字的字体大小，图片空白区不能超过70%、文字可以换行和旋转角度
+
+class TextToImg
+{
+    private $image; // 图片资源
+    private $font_size  = 14; // 字体大小
+    private $font_color; // 字体颜色
+    private $bg_color; // 背景颜色
+    private $font_angle = 0; // 字体旋转角度
+    private $font_file; // 字体文件
+    private $text; // 文字内容
+    private $textArea   = 0.9; // 文字区域
+
+    public function __construct($width, $height)
+    {
+        $this->image = imagecreatetruecolor($width, $height); // 创建指定大小的图片资源
+    }
+
+    public function setFontSize($size)
+    {
+        $this->font_size = $size; // 设置字体大小
+        return $this;
+    }
+
+    public function setFontColor($color)
+    {
+        $this->font_color = $color; // 设置字体颜色
+        return $this;
+    }
+
+    public function setBgColor($color)
+    {
+        $this->bg_color = $color; // 设置背景颜色
+        return $this;
+    }
+
+    public function setTextArea($area = 0.9)
+    {
+        $this->textArea = $area; // 设置文字宽度最多不能超过多少比例，例如 0.8 表示文字的最大宽度为图片宽度的 80%
+        return $this;
+    }
+
+    public function setFontAngle($angle = 0)
+    {
+        $this->font_angle = $angle; // 设置字体旋转角度
+        return $this;
+    }
+
+    public function setFontFile($file)
+    {
+        $this->font_file = $file; // 设置字体文件
+        return $this;
+    }
+
+    public function setText($text)
+    {
+        $this->text = $text; // 设置文字内容
+        return $this;
+    }
+
+    public function generate()
+    {
+        $bg_color = imagecolorallocate($this->image, $this->bg_color[0], $this->bg_color[1], $this->bg_color[2]); // 创建背景颜色
+        imagefill($this->image, 0, 0, $bg_color); // 填充背景颜色
+
+        $font_color = imagecolorallocate($this->image, $this->font_color[0], $this->font_color[1], $this->font_color[2]); // 创建字体颜色
+
+        $box         = imagettfbbox($this->font_size, $this->font_angle, $this->font_file, $this->text); // 获取字体的边界框
+        $text_width  = abs($box[4] - $box[0]); // 计算字体的宽度
+        $text_height = abs($box[5] - $box[1]); // 计算字体的高度
+
+        $max_text_width = $this->getWidth() * $this->textArea; // 计算最大的字体宽度，不能超过图片宽度的 70%
+        if ($text_width > $max_text_width) {
+            $this->font_size = $this->font_size * ($max_text_width / $text_width); // 根据最大宽度调整字体大小
+            $box             = imagettfbbox($this->font_size, $this->font_angle, $this->font_file, $this->text); // 重新获取字体的边界框
+            $text_width      = abs($box[4] - $box[0]); // 重新计算字体的宽度
+            $text_height     = abs($box[5] - $box[1]); // 重新计算字体的高度
+        }
+
+        $words = explode(' ', $this->text); // 将文字内容按空格分割成单词
+        $lines = [];
+        $line  = '';
+        foreach ($words as $word) {
+            $box        = imagettfbbox($this->font_size, $this->font_angle, $this->font_file, $line . ' ' . $word); // 获取当前行加上下一个单词后的边界框
+            $line_width = abs($box[4] - $box[0]); // 计算当前行加上下一个单词后的宽度
+            if ($line_width > $max_text_width) { // 如果当前行加上下一个单词后的宽度超过最大宽度
+                $lines[] = $line; // 将当前行加入行数组
+                $line    = $word; // 开始新的一行
+            } else {
+                $line .= ' ' . $word; // 继续在当前行添加单词
+            }
+        }
+        $lines[] = $line; // 将最后一行加入行数组
+
+        foreach ($lines as $i => $line) {
+            $box        = imagettfbbox($this->font_size, $this->font_angle, $this->font_file, $line); // 获取当前行的边界框
+            $text_width = abs($box[4] - $box[0]); // 计算当前行的宽度
+            $x          = ($this->getWidth() - $text_width) / 2; // 计算当前行的横向位置
+            $y          = ($this->getHeight() - $text_height) / 2 + $text_height + $i * $text_height * 1.2; // 计算当前行的纵向位置
+            imagettftext($this->image, $this->font_size, $this->font_angle, (int)$x, (int)$y, $font_color, $this->font_file, $line); // 在图片上绘制文字
+        }
+        return $this;
+    }
+
+    public function getWidth()
+    {
+        return imagesx($this->image); // 获取图片宽度
+    }
+
+    public function getHeight()
+    {
+        return imagesy($this->image); // 获取图片高度
+    }
+
+    public function output()
+    {
+        header('Content-Type: image/png'); // 设置输出类型为 PNG 图片
+        imagepng($this->image); // 输出图片
+        imagedestroy($this->image); // 销毁图片资源
+        die;
+    }
+}
+
+// 使用这个类，可以按照以下步骤生成文字图片：
+
+$textToImage = new TextToImage(400, 200); // 创建一个 400x400 的图片
+$textToImage->setFontSize(35); // 设置字体大小为 24
+$textToImage->setFontColor([255, 255, 255]); // 设置字体颜色为白色
+$textToImage->setBgColor([0, 0, 0]); // 设置背景颜色为黑色
+// $textToImage->setFontAngle(45); // 设置字体旋转角度为 45 度
+$textToImage->setFontFile('./ali_puhui.ttf'); // 设置文字字体
+$textToImage->setText('Hello, World!'); // 设置内容
+$textToImage->generate(); // 生成图片
+$textToImage->output(); // 输出图片
+// 这个示例代码会生成一个 400x400 的黑色背景的图片，上面写着 "Hello, World!"，字体颜色为白色，字体旋转角度为 45 度。

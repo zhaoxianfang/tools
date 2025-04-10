68d7f0a2d60b12a2d04d48c15d2a8536f5a530ef
zhaoxianfang <1748331509@qq.com>
2025-04-10 10:49:34 +0800

    fix

--- config/tools_other.php
+++ config/tools_other.php
@@ -4,7 +4,7 @@
 
 return [
     // 截图程序
-    'phantomjs' => env('TOOLS_PHANTOMJS_PATH', ''), // 'phantomjs.exe' 或者 'phantomjs' 存放的绝对路径 例如 /www/soft
+    'phantomjs' => env('OAUTH_PHANTOMJS_PATH', ''), // 'phantomjs.exe' 或者 'phantomjs' 存放的绝对路径 例如 /www/soft
 
     // 默认缓存路径
     'cache_path' => is_laravel() ? config('cache.stores.file.path', storage_path('framework/cache/data')) : __DIR__.'/cache',
@@ -18,14 +18,14 @@ return [
     // 短信通知
     'sms' => [
         'aliyun' => [
-            'app_id' => env('TOOLS_SMS_ALI_APP_ID', ''), // accessKeyId
-            'secret' => env('TOOLS_SMS_ALI_SECRET', ''), // accessKeySecret
-            'sign' => env('TOOLS_SMS_ALI_SIGN', ''), // 签名
+            'app_id' => env('OAUTH_SMS_ALI_APP_ID', ''), // accessKeyId
+            'secret' => env('OAUTH_SMS_ALI_SECRET', ''), // accessKeySecret
+            'sign' => env('OAUTH_SMS_ALI_SIGN', ''), // 签名
         ],
         'tencent' => [
-            'app_id' => env('TOOLS_SMS_TENCENT_APP_ID', ''), // accessKeyId
-            'secret' => env('TOOLS_SMS_TENCENT_SECRET', ''), // accessKeySecret
-            'sign' => env('TOOLS_SMS_TENCENT_SIGN', ''), // 签名
+            'app_id' => env('OAUTH_SMS_TENCENT_APP_ID', ''), // accessKeyId
+            'secret' => env('OAUTH_SMS_TENCENT_SECRET', ''), // accessKeySecret
+            'sign' => env('OAUTH_SMS_TENCENT_SIGN', ''), // 签名
         ],
         // ...
     ],
